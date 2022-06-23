7f8302f55a6a22526591664fc5638d14c58cece9
Maikol Jimenez <42153992+MaikJim@users.noreply.github.com>
2022-06-23 16:31:38 -0400

    Update index.js

--- SEARCH_JS/index.js
+++ SEARCH_JS/index.js
@@ -13,6 +13,11 @@ document.getElementById("searchInput").addEventListener("keyup", function(event)
     
     for (let counter = 0; counter < allNamesDOMCollection.length; counter++) {
         const currentName = allNamesDOMCollection[counter].textContent.toLowerCase();
-        console.log(currentName);
+        
+        if (currentName.includes(searchQuery)) {
+            allNamesDOMCollection[counter].style.display = "block";
+        } else {
+            allNamesDOMCollection[counter].style.display = "none";
+        }
     }
 });
